57bc45f791439a3befdff3a46e9f2552038e2348
Logeshwari <logeshwaripandian465@gmail.com>
2023-10-30 22:07:26 +0530

    fetched data from backend api

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -5,22 +5,11 @@ import DropDownPicker from "react-native-dropdown-picker";
 const DropDown = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [currentValue, setCurrentValue] = useState(null);
-  // const companies = [
-  //   { label: "company1", value: "company1" },
-  //   { label: "company2", value: "company2" },
-  //   { label: "company3", value: "company3" },
-  //   { label: "company4", value: "company4" },
-  //   { label: "company5", value: "company5" },
-  //   { label: "company6", value: "company6" },
-  //   { label: "company7", value: "company7" },
-  //   { label: "company8", value: "company8" },
-  //   { label: "company9", value: "company9" },
-  //   { label: "company10", value: "company10" },
-  // ];
+
   const [clients, setClients] = useState([]);
 
   useEffect(() => {
-    fetch("http://192.168.141.152:3000/api/v1/clients/")
+    fetch("http://10.11.49.136:3000/api/v1/clients/")
       .then((response) => response.json())
       .then((res) => setClients(res.data.clients))
       .catch((err) => console.log(err));
@@ -28,7 +17,10 @@ const DropDown = () => {
   return (
     <View className="flex-1 p-20 mt-20">
       <DropDownPicker
-        items={clients}
+        items={clients.map((client) => ({
+          label: client.businessName,
+          value: client._id,
+        }))}
         open={isOpen}
         setOpen={() => setIsOpen(!isOpen)}
         value={currentValue}
--- src/screens/ViewClients.js
+++ src/screens/ViewClients.js
@@ -1,40 +1,43 @@
-import { Text, View, TouchableOpacity } from 'react-native';
-import Head from '../components/Head';
-import ClientComponent from '../components/ClientComponent';
-import { useEffect, useState } from 'react';
+import { Text, View, TouchableOpacity } from "react-native";
+import Head from "../components/Head";
+import ClientComponent from "../components/ClientComponent";
+import { useEffect, useState } from "react";
 // import axios from 'axios';
 
 export default function ViewClients() {
-	// const clients = ['Google', 'Amazon', 'flipkart', 'Hyundai'];
-	const [clients, setClients] = useState([]);
+  // const clients = ['Google', 'Amazon', 'flipkart', 'Hyundai'];
+  const [clients, setClients] = useState([]);
 
-	useEffect(() => {
-		fetch('http://192.168.141.152:3000/api/v1/clients/')
-			.then((response) => response.json())
-			.then((res) => setClients(res.data.clients))
-			.catch((err) => console.log(err));
-	}, []);
+  useEffect(() => {
+    fetch("http://10.11.49.136:3000/api/v1/clients/")
+      .then((response) => response.json())
+      .then((res) => setClients(res.data.clients))
+      .catch((err) => console.log(err));
+  }, []);
 
-	return (
-		<View className="w-screen">
-			<Head />
-			<View className="w-screen flex items-end">
-				<Text className="mt-8 text-lg mr-12">List of Clients</Text>
-			</View>
-			<View className="w-screen flex items-center mt-3">
-				<View className="w-11/12">
-					<View className="w-full flex items-center relative">
-						{clients.map((client) => (
-							<ClientComponent key={client._id} clientName={client.businessName} />
-						))}
-					</View>
-					<View className="absolute top-full left-10 mt-4">
-						<TouchableOpacity className="bg-blue-500 p-3 rounded-lg">
-							<Text className="text-white">Add Client</Text>
-						</TouchableOpacity>
-					</View>
-				</View>
-			</View>
-		</View>
-	);
+  return (
+    <View className="w-screen">
+      <Head />
+      <View className="w-screen flex items-end">
+        <Text className="mt-8 text-lg mr-12">List of Clients</Text>
+      </View>
+      <View className="w-screen flex items-center mt-3">
+        <View className="w-11/12">
+          <View className="w-full flex items-center relative">
+            {clients.map((client) => (
+              <ClientComponent
+                key={client._id}
+                clientName={client.businessName}
+              />
+            ))}
+          </View>
+          <View className="absolute top-full left-10 mt-4">
+            <TouchableOpacity className="bg-blue-500 p-3 rounded-lg">
+              <Text className="text-white">Add Client</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
 }
