4fad34f4ceb8ee42fa2d8fa4ec259668a6e5b0e5
Logeshwari <logeshwaripandian465@gmail.com>
2023-10-30 11:05:02 +0530

    created dropdown component

--- src/screens/AddOrders.js
+++ src/screens/AddOrders.js
@@ -1,41 +1,54 @@
-import { Text, TouchableOpacity, View } from 'react-native';
-import Head from './../components/Head';
-import { useState } from 'react';
-import DateComponent from '../components/DateComponent';
-import MenuComponent from './../components/MenuComponent';
+import { Text, TouchableOpacity, View } from "react-native";
+import Head from "./../components/Head";
+import { useState } from "react";
+import DateComponent from "../components/DateComponent";
+import MenuComponent from "./../components/MenuComponent";
+import DropDown from "../components/DropDown";
 
 export default AddOrders = () => {
-	const [isCalendarOpen, setIsCalendarOpen] = useState(false);
-	const handleOpenCalendar = () => {
-		setIsCalendarOpen(!isCalendarOpen);
-	};
+  const [isCalendarOpen, setIsCalendarOpen] = useState(false);
+  const handleOpenCalendar = () => {
+    setIsCalendarOpen(!isCalendarOpen);
+  };
 
-	const [selectedStartDate, setSelectedStartDate] = useState('');
+  const [selectedStartDate, setSelectedStartDate] = useState("");
 
-	const menu = ['breakfast', 'lunch', 'dinner', 'meal', 'tea/coffee'];
+  const menu = ["breakfast", "lunch", "dinner", "meal", "tea/coffee"];
 
-	return (
-		<View>
-			<Head />
-			<View className="ml-8">
-				<Text className="text-base">Select Date</Text>
-				<TouchableOpacity className="border w-4/5 p-3 mt-2" onPress={handleOpenCalendar}>
-					<Text>{selectedStartDate}</Text>
-				</TouchableOpacity>
-				<DateComponent isCalendarOpen={isCalendarOpen} setSelectedStartDate={setSelectedStartDate} handleOpenCalendar={handleOpenCalendar} />
-				<View>
-					<View className="flex flex-row justify-between py-2 px-10 bg-zinc-300 w-11/12 mt-9 border-b">
-						<Text className="text-lg">Item</Text>
-						<Text className="text-lg">Quantity</Text>
-					</View>
-					{menu.map((menuItem) => (
-						<MenuComponent menuItem={menuItem} />
-					))}
-				</View>
-				<TouchableOpacity className="bg-blue-400 w-20 p-2 rounded-md mt-3">
-					<Text className="text-center text-white">submit</Text>
-				</TouchableOpacity>
-			</View>
-		</View>
-	);
+  return (
+    <View>
+      <Head />
+
+      <View>
+        <DropDown />
+        <View className="ml-8">
+          <Text className="text-base">Select Date</Text>
+          <TouchableOpacity
+            className="border w-4/5 p-3 mt-2"
+            onPress={handleOpenCalendar}
+          >
+            <Text>{selectedStartDate}</Text>
+          </TouchableOpacity>
+
+          <DateComponent
+            isCalendarOpen={isCalendarOpen}
+            setSelectedStartDate={setSelectedStartDate}
+            handleOpenCalendar={handleOpenCalendar}
+          />
+          <View>
+            <View className="flex flex-row justify-between py-2 px-10 bg-zinc-300 w-11/12 mt-9 border-b">
+              <Text className="text-lg">Item</Text>
+              <Text className="text-lg">Quantity</Text>
+            </View>
+            {menu.map((menuItem) => (
+              <MenuComponent menuItem={menuItem} />
+            ))}
+          </View>
+          <TouchableOpacity className="bg-blue-400 w-20 p-2 rounded-md mt-3">
+            <Text className="text-center text-white">submit</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
 };
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { TouchableOpacity, View, Text } from "react-native";
+import { TouchableOpacity, View, Text, ScrollView } from "react-native";
 import Head from "../components/Head";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 
@@ -13,17 +13,21 @@ export default function HomeScreen({ navigation }) {
     { label: "Generate Invoice", icon: "book", to: "IRgeneration" },
   ];
 
-	return (
-		<View>
-			<Head />
-			<View className="w-full flex flex-row items-center flex-wrap content-start mt-4 ">
-				{menu.map((item, i) => (
-					<TouchableOpacity className="bg-gray-300 p-4 m-6 w-32 h-32 justify-items-center items-center justify-end rounded-md" onPress={() => navigation.navigate(item.to)} key={i}>
-						<FontAwesome name={item.icon} size={32} />
-						<Text className="text-center mt-2">{item.label}</Text>
-					</TouchableOpacity>
-				))}
-			</View>
-		</View>
-	);
+  return (
+    <ScrollView>
+      <Head />
+      <View className="w-full flex flex-row items-center flex-wrap content-start mt-4 ">
+        {menu.map((item, i) => (
+          <TouchableOpacity
+            className="bg-gray-300 p-4 m-6 w-32 h-32 justify-items-center items-center justify-end rounded-md"
+            onPress={() => navigation.navigate(item.to)}
+            key={i}
+          >
+            <FontAwesome name={item.icon} size={32} />
+            <Text className="text-center mt-2">{item.label}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    </ScrollView>
+  );
 }
