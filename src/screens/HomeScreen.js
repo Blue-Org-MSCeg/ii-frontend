2fe90fce3a3ef5826d8374d4a288c500febc648c
charusara <charusara13@gmail.com>
2023-11-01 16:41:07 +0530

    added menu quotation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -13,17 +13,21 @@ export default function HomeScreen({ navigation }) {
     { id: "7", label: "Generate Invoice", icon: "book", to: "IRgeneration" },
   ];
 
-	return (
-		<View>
-			<Head />
-			<View className="w-full flex flex-row items-center flex-wrap content-start mt-4 ">
-				{menu.map((item, i) => (
-					<TouchableOpacity className="bg-gray-300 p-4 m-6 w-32 h-32 justify-items-center items-center justify-end rounded-md" onPress={() => navigation.navigate(item.to)} key={i}>
-						<FontAwesome name={item.icon} size={32} />
-						<Text className="text-center mt-2">{item.label}</Text>
-					</TouchableOpacity>
-				))}
-			</View>
-		</View>
-	);
+  return (
+    <ScrollView>
+      <Head />
+      <View className="w-full flex flex-row items-center flex-wrap content-start mt-4 ">
+        {menu.map((item) => (
+          <TouchableOpacity
+            keu={item.id}
+            className="bg-gray-300 p-4 m-6 w-32 h-32 justify-items-center items-center justify-end rounded-md"
+            onPress={() => navigation.navigate(item.to)}
+          >
+            <FontAwesome name={item.icon} size={32} />
+            <Text className="text-center mt-2">{item.label}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    </ScrollView>
+  );
 }
