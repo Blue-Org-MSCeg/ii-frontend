3206d75e14549ae5767fcf3619ac3e5bfa565c9d
Raja <srajasep27@gmail.com>
2023-11-01 18:27:41 +0530

    removed comments in menuQuotation page

--- src/screens/MenuQuotation.js
+++ src/screens/MenuQuotation.js
@@ -15,9 +15,7 @@ export default function MenuQuotation() {
 	const [food, setFood] = useState('');
 	const [itemEdit, setItemEdit] = useState(null);
 	const [isAddFoodOpen, setIsAddFoodOpen] = useState(false);
-
 	const [isEditFoodOpen, setIsEditFoodOpen] = useState(false);
-	// const [editedItemCost, setEditedItemCost] = useState("");
 
 	const editorPass = (item) => {
 		setItemEdit(item);
@@ -44,12 +42,6 @@ export default function MenuQuotation() {
 		toggleAddFoodHandler();
 	};
 
-	// useEffect(() => {
-	// 	const updatedItem = menuItem.map((item) => (item.id === itemEdit.food ? { id: itemEdit.id, food: itemEdit.food, cost: itemEdit.cost } : item));
-	// 	setMenuItem(updatedItem);
-	// 	setIsEditFoodOpen(false);
-	// }, [itemEdit]);
-
 	useEffect(() => {
 		console.log(itemEdit);
 		if (itemEdit) {
@@ -59,32 +51,13 @@ export default function MenuQuotation() {
 		}
 	}, [itemEdit]);
 
-	// const handleEdit = () => {
-	//   const updatedItem = menuItem.map((item) =>
-	//     item.food === isEditingFood ? { ...item, cost: editedItemCost } : item
-	//   );
-	//   setMenuItem(updatedItem);
-	//   setIsEditFoodOpen("");
-	//   setEditedItemCost("");
-	// };
-
-	// const handleRemove = (index) => {
-	//   const newOptions = [...menuItem];
-	//   newOptions.splice(index, 1);
-	//   setMenuItem(newOptions);
-	// };
-
-	// const handleEditComponent = (isEditFoodOpen) => {
-	//   setIsEditFoodOpen(isEditFoodOpen);
-	// };
-
 	return (
 		<View className="w-full">
 			<View className="mt-10 mb-8 p-5 border-solid content-center border-1 justify-center bg-blue-400 ">
 				<Text className="text-center">MenuQuotation</Text>
 			</View>
-			{/* add food items for company */}
 
+			{/* add food items for company */}
 			{isAddFoodOpen && (
 				<View className="align-middle justify-center content-centers flex border-b mb-5">
 					<View className="flex-row flex-2 p-15">
@@ -108,26 +81,7 @@ export default function MenuQuotation() {
 				</View>
 			)}
 
-			{isEditFoodOpen && (
-				<EditerComponent itemEdit={itemEdit} setItemEdit={setItemEdit} />
-				// <View className="justify-between mt-5 mb-10 border-b">
-				//   <View className="bg-gray-300 w-52 justify-center content-center p-3 mb-5 mx-20 ml-30">
-				//     <Text className="text-center ">Edit Cost</Text>
-				//   </View>
-				//   <View className="flex-row justify-center mb-5">
-				//     <TextInput
-				//       className="border-2 border-black w-20 mr-10"
-				//       value={editedItemCost}
-				//       onChangeText={setEditedItemCost}
-				//       placeholder="Cost"
-				//       keyboardType="numeric"
-				//     />
-				//     <View className="ml-2">
-				//       <Button title="Save" onPress={handleEdit} />
-				//     </View>
-				//   </View>
-				// </View>
-			)}
+			{isEditFoodOpen && <EditerComponent itemEdit={itemEdit} setItemEdit={setItemEdit} />}
 
 			{/* View menu */}
 			<View className="place-items-center">
@@ -141,27 +95,6 @@ export default function MenuQuotation() {
 				</View>
 				{menuItem.map((item) => (
 					<EditFoodComponent item={item} index={item.id} setIsEditFoodOpen={setIsEditFoodOpen} setMenuItem={setMenuItem} editorPass={editorPass} />
-					// <View className=" flex-row mb-3 justify-between mr-2" key={index}>
-					//   <View className="border border-black p-2 m-1 w-40 bg-gray-300">
-					//     <Text>{item.food}</Text>
-					//   </View>
-					//   <View className="border border-black p-2 m-1 w-20 bg-gray-300">
-					//     <Text className=" text-center ">{item.cost}</Text>
-					//   </View>
-
-					//   <View className="mr-2 m-1">
-					//     <Button
-					//       title="edit"
-					//       onPress={() => {
-					//         setIsEditFoodOpen(item.food);
-					//         setEditedItemCost(item.cost);
-					//       }}
-					//     />
-					//   </View>
-					//   <View className="m-1">
-					//     <Button title="remove" onPress={() => handleRemove(index)} />
-					//   </View>
-					// </View>
 				))}
 			</View>
 
