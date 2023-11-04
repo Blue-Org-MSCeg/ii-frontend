74ba2c6a288daac037a375425b78d0f60d90ea9b
himanesh21 <dodgerrocks770@gmail.com>
2023-11-04 18:04:23 +0530

    edit order ui page completed

--- src/components/EditRemove.js
+++ src/components/EditRemove.js
@@ -1,24 +1,86 @@
 import { FontAwesome } from '@expo/vector-icons';
 import { useState } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { Text, TextInput, View , TouchableOpacity} from 'react-native';
 
-export default MenuComponent = (props) => {
-	const [quantity, setQuantity] = useState('0');
 
-	function addQuantityHandler() {
-		const newQuanity = (parseInt(quantity) + 1).toString();
-		setQuantity(newQuanity);
-	}
+export default EditRemove = ({food,fquantity, updateOrder}) => {
+	const [isEditing, setIsEditing] = useState(false);
+    const [newQuantity, setNewQuantity] = useState(fquantity.toString());
+	// const [quantity, setQuantity] = useState('0');
+
+	// function addQuantityHandler() {
+	// 	const newQuanity = (parseInt(quantity) + 1).toString();
+	// 	setQuantity(newQuanity);
+	// }
+	// function subtractQuantityHandler() {
+	// 	const newQuanity = (parseInt(quantity) - 1).toString();
+	// 	if (newQuanity >= 0) setQuantity(newQuanity);
+	// }
+	const handleEdit = () => {
+		setIsEditing(true);
+	  };
+	
+	  const handleSave = () => {
+		const newQuantityInt = parseInt(newQuantity, 10);
+    // const newQuantityInt = parseInt(newQuantity, 10);
+    if (!isNaN(newQuantityInt) && newQuantityInt >= 0 && newQuantityInt === parseFloat(newQuantity)) {
+      // Update the order array with the new quantity
+      updateOrder(food, newQuantityInt);
+      setIsEditing(false); // Exit "Edit" mode
+    } else {
+      alert('Invalid quantity. Please enter a non-negative integer.');
+    }
+	  };
 
 	return (
-		<View key={props.menuItem} className="flex-row justify-between items-center py-2 px-10 bg-zinc-300 w-11/12">
-			<Text className="text-lg">{props.menuItem}</Text>
-			{/* increase and decrease quantity */}
-			<View className="flex-row justify-between w-28 items-center">
-				<FontAwesome name="minus" size={20} color="gray" />
-				<TextInput keyboardType="numeric" className="bg-white w-12 h-7 rounded-md text-center" value={quantity} onChangeText={(noOfHeads) => setQuantity(noOfHeads)}></TextInput>
-				<FontAwesome name="plus" size={20} color="gray" onPress={addQuantityHandler} />
-			</View>
-		</View>
-	);
+	// 	<View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 10, paddingVertical: 8, borderBottomWidth: 1 }}>
+    //   <Text style={{ fontSize: 16 }}>{food}</Text>
+	//   {/* <TouchableOpacity onPress={() => subtractQuantityHandler}>
+    //       <Text style={{ fontSize: 20, marginHorizontal: 10 }}>-</Text>
+    //     </TouchableOpacity> */}
+	// 	 <Text style={{ fontSize: 16 }}>{fquantity}</Text>
+    //   <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+    //     {/* <Text style={{ fontSize: 16 }}>{fquantity}</Text> */}
+    //     <TouchableOpacity onPress={() => addQuantityHandler}>
+    //       <Text style={{ fontSize: 20, marginHorizontal: 10 }}>+</Text>
+    //     </TouchableOpacity>
+    //   </View>
+    // </View>
+	<View
+    className ="flex-row justify-between items-center px-3 py-2 border-b"
+      // style={{
+      //   flexDirection: 'row',
+      //   justifyContent: 'space-between',
+      //   alignItems: 'center',
+      //   paddingHorizontal: 10,
+      //   paddingVertical: 8,
+      //   borderBottomWidth: 1,
+      // }}
+    >
+      {/* style={{ fontSize: 16 }} */}
+      <Text className="text-2xl font-normal">{food}</Text>
+      {isEditing ? (
+        <View className="flex-row items-center justify-center">
+          <TextInput
+            value={newQuantity}
+            onChangeText={(text) => setNewQuantity(text)}
+            keyboardType="numeric"
+          />
+          <TouchableOpacity onPress={handleSave}>
+            <Text className="bg-blue-400 w-15 p-1 rounded-md mt-2 items-center">Save</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <View className="flex flex-row items-center justify-center">
+          <Text className="text-lg mr-2 border px-3 rounded-md">{fquantity}</Text>
+          <TouchableOpacity onPress={handleEdit}>
+            <Text className="bg-green-400 w-15 p-1 rounded-md items-center px-3 py-1 ">Edit</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="px-3 py-1 rounded-full flex justify-center items-center">
+				<FontAwesome name="minus" color="red" size={15} />
+			</TouchableOpacity>
+        </View>
+      )}
+    </View>
+  );
 };
--- src/screens/EditOrder.js
+++ src/screens/EditOrder.js
@@ -4,6 +4,7 @@ import DropDown from "../components/DropDown";
 import Head from "../components/Head";
 import DateComponent from "../components/DateComponent";
 import EditRemove from "../components/EditRemove";
+import MenuComponent from "../components/MenuComponent"; // Make sure to import MenuComponent
 
 export default function EditOrder() {
   const [isCalendarOpen, setIsCalendarOpen] = useState(false);
@@ -11,47 +12,64 @@ export default function EditOrder() {
     setIsCalendarOpen(!isCalendarOpen);
   };
   const [selectedStartDate, setSelectedStartDate] = useState("");
-  const order = [
-    {food:"breakfast",quantity:30},
-    {food:"lunch",quantity:20},
-    {food:"dinner",quantity:15},
-    {food:"meal",quantity:45},
-    {food:"tea/coffee",quantity:8}
-  ];
-  const menu = ["breakfast", "lunch", "dinner", "meal", "tea/coffee"];
-  return (
-    <View className='flex-1 bg-red-500'>
-      <Head/>
-    <View className="bg-black border-b-4"><DropDown/></View>
-    <View className="ml-8">
-          <Text className="text-base">Select Date</Text>
-          <TouchableOpacity
-            className="border w-4/5 p-3 mt-2"
-            onPress={handleOpenCalendar}
-          >
-            <Text>{selectedStartDate}</Text>
-          </TouchableOpacity>
-
-          <DateComponent
-            isCalendarOpen={isCalendarOpen}
-            setSelectedStartDate={setSelectedStartDate}
-            handleOpenCalendar={handleOpenCalendar}
-          />
-          <View>
-            <View className="flex flex-row justify-between py-2 px-10 bg-zinc-300 w-11/12 mt-9 border-b">
-              <Text className="text-lg">Item</Text>
-              <Text className="text-lg">Quantity</Text>
-            </View>
-            {menu.map((menuItem) => {
-              <MenuComponent menuItem={menuItem} />
-              //<EditRemove menuItem={menuItem.food}/>
-})}
-          </View>
-          <TouchableOpacity className="bg-blue-400 w-20 p-2 rounded-md mt-3">
-            <Text className="text-center text-white">submit</Text>
-          </TouchableOpacity>
-        </View>
-    </View>
+  const [order, setOrder] = useState( [
+    { food: "breakfast", quantity: 30 },
+    { food: "lunch", quantity: 20 },
+    { food: "dinner", quantity: 15 },
+    { food: "meal", quantity: 45 },
+    { food: "tea/coffee", quantity: 8 },
     
+  ]);
+  const updateOrder = (food, newQuantity) => {
+    // Find the index of the menu item in the order array
+    const itemIndex = order.findIndex((menuItem) => menuItem.food === food);
+
+    if (itemIndex !== -1) {
+      // Clone the order array to avoid mutating state directly
+      const updatedOrder = [...order];
+
+      // Update the quantity for the specific menu item
+      updatedOrder[itemIndex].quantity = newQuantity;
+
+      // Set the state with the updated order array
+      setOrder(updatedOrder); // Assuming you have a state variable 'order' in your parent component
+    }
+  };
+
+  return (
+    <View className="flex-1 bg-white-500">
+      <Head />
+      <View className="">
+        <DropDown />
+      </View>
+      <View className="ml-8 mt-20">
+        <Text className="text-base">Select Date</Text>
+        <TouchableOpacity
+          className="border w-4/5 p-3 mt-2"
+          onPress={handleOpenCalendar}
+        >
+          <Text>{selectedStartDate}</Text>
+        </TouchableOpacity>
+
+        <DateComponent
+          isCalendarOpen={isCalendarOpen}
+          setSelectedStartDate={setSelectedStartDate}
+          handleOpenCalendar={handleOpenCalendar}
+        />
+        <View>
+          <View className="flex flex-row justify-between py-2 px-10 bg-zinc-300 w-11/12 mt-9 border-b">
+            <Text className="text-lg">Item</Text>
+            <Text className="text-lg">Quantity</Text>
+          </View>
+          {order.map((menuItem, index) => (
+      <EditRemove key={index} food={menuItem.food} fquantity={menuItem.quantity} updateOrder={updateOrder} />
+    ))}
+        </View>
+        {/* <TouchableOpacity className="bg-blue-400 w-20 p-2 rounded-md mt-3">
+          <Text className="text-center text-white">submit</Text>
+        </TouchableOpacity> */}
+      </View>
+    </View>
   );
 }
+
