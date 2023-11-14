7cb8e8f516687354eed694a1c53bc7d82520f5f7
Raja <srajasep27@gmail.com>
2023-11-14 23:47:30 +0530

    integrated invoice api but some bug is there

--- src/screens/IRgeneration.js
+++ src/screens/IRgeneration.js
@@ -3,7 +3,7 @@ import { View, Text, Button } from 'react-native';
 import DropDown from '../components/DropDown';
 import { shareAsync } from 'expo-sharing';
 import { printToFileAsync } from 'expo-print';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const data = {
 	bankname: 'UNION BANK OF INDIA',
@@ -14,100 +14,95 @@ const data = {
 };
 
 const IRgeneration = () => {
-	const [client, setClient] = useState({});
+	const [client, setClient] = useState({ address: '', items: [] });
+	const [invoice, setInvoice] = useState([]);
+	let totalAmount = 0;
 	const htmlContent = `
-  <html>
-  <head>
-  <meta charset="utf-8">
-  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-  </head>
-  <body>
-      <header>
-      <br><br><br><br><br><br><br><br><br>
-      <div class="flex flex-col space-x-4">
-
-      <!-- Right-aligned Flex Item -->
-      <div class="flex-1 text-right">
-      <pre>
-      DATE : ${new Date().toLocaleDateString().split(' ')[0].padEnd(24)}
-      ${data.bankname.padEnd(30)} 
-      ${data.accountno.padEnd(31)}
-      ${data.ifsc.padEnd(30)} 
-      ${data.gst.padEnd(30)} 
-      ${data.hssonno.padEnd(31)}
-      </pre>
-      </div>
-      <div class="flex-1 bg-green-200 p-4 text-center">
-      <h3>INVOICE</h3>
-      </div>
-      <!-- Left-aligned Flex Item -->
-      <div class="flex-1 bg-blue-200 p-4">
-      ${client.address ? client.address.split(',').join('<br>') : ''}
-      </div>
-
-      <!-- Center-aligned Flex Item -->
-      <div class="flex-1 bg-green-200 p-4 text-center">
-      <table class="w-full mt-4">
-          <thead>
-            <tr>
-              <th class="border">S.No</th>
-              <th class="border">Item</th>
-              <th class="border">Rate</th>
-              <th class="border">Quantity</th>
-              <th class="border">Amount</th>
-            </tr>
-          </thead>
-          <tbody>;
-  ${client.items
-		.map((item, index) => {
-			const amount = item.cost * item.quantity;
-			return `<tr key=${item._id}>
-                      <td className="border">${index + 1}</td>
-                      <td className="border">${item._id}</td>
-                      <td className="border">${item.cost}</td>
-                      <td className="border">${item.quantity}</td>
-                      <td className="border">${item.amount}</td>
-                    </tr>`;
-		})
-		.join(' ')}
-  </tbody>
-      </table>
-      <div className="mt-4">
-        <strong>Total Amount:</strong> {totalAmount}
-      </div>
-    </div>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+        </head>
+        <body>
+          <header>
+            <br><br><br><br><br><br><br><br><br>
+            <div class="flex flex-col space-x-4">
+              <!-- Right-aligned Flex Item -->
+              <div class="flex-1 text-right">
+                <pre>
+                  DATE : ${new Date().toLocaleDateString().split(' ')[0].padEnd(24)}
+                  ${data.bankname.padEnd(30)} 
+                  ${data.accountno.padEnd(31)}
+                  ${data.ifsc.padEnd(30)} 
+                  ${data.gst.padEnd(30)} 
+                  ${data.hssonno.padEnd(31)}
+                </pre>
+              </div>
+              <div class="flex-1 bg-green-200 p-4 text-center">
+                <h3>INVOICE</h3>
+              </div>
+              <!-- Left-aligned Flex Item -->
+              <div class="flex-1 bg-blue-200 p-4">
+                ${client.businessName}<br>
+                ${client.address ? client.address.split(',').join('<br>') : ''}
+              </div>
     
-    
-    
-    
-    
-    
-    
-    
-      
-        </tbody>
-      </table>
-      </div>
-  </div>
-  </body>
-  </html>
-`;
+              <!-- Center-aligned Flex Item -->
+              <div class="flex-1 bg-green-200 p-4 text-center">
+                <table class="w-full mt-4">
+                  <thead>
+                    <tr>
+                      <th class="border">S.No</th>
+                      <th class="border">Item</th>
+                      <th class="border">Rate</th>
+                      <th class="border">Quantity</th>
+                      <th class="border">Amount</th>
+                    </tr>
+                  </thead>
+                  <tbody>;
+                    ${invoice
+											.map((item, index) => {
+												totalAmount += item.amount;
+												return `<tr key=${item._id}>
+                                        <td className="border">${index + 1}</td>
+                                        <td className="border">${item._id}</td>
+                                        <td className="border">${item.cost}</td>
+                                        <td className="border">${item.quantity}</td>
+                                        <td className="border">${item.amount}</td>
+                                      </tr>`;
+											})
+											.join(' ')}
+                  </tbody>
+                </table>
+                <div className="mt-4">
+                  <strong>Total Amount:</strong> ${totalAmount}
+                </div>
+              </div>
+               </tbody>
+               </table>
+               </div>
+            </div>
+        </body>
+      </html>
+    `;
 
 	// get client details
 	const changeOrderList = (client) => {
-		console.log(client.address);
 		setClient(client);
 	};
 
 	const generatePDF = async () => {
-		fetch(`http:192.168.120.16:3000/api/v1/orders/invoice/${client.businessName}/2023-10-20/2023-10-31`)
+		fetch(`http:192.168.137.1:3000/api/v1/orders/invoice/${client.businessName}/2023-10-20/2023-10-31`)
 			.then((response) => {
 				if (!response.ok) {
 					throw new Error(`HTTP error! status: ${response.status}`);
 				}
 				return response.json();
 			})
-			.then((data) => console.log(data.data.invoice))
+			.then((data) => {
+				setInvoice(data.data.invoice);
+				console.log(data.data.invoice);
+			})
 			.catch((err) => console.log(err));
 
 		const file = await printToFileAsync({
@@ -116,6 +111,8 @@ const IRgeneration = () => {
 		});
 		await shareAsync(file.uri);
 	};
+
+	useEffect(() => {}, [htmlContent]);
 
 	return (
 		<View className="flex-1">
@@ -127,7 +124,7 @@ const IRgeneration = () => {
 				</View>
 				<View className="w-4" />
 				<View className="py-2 px-4 rounded">
-					<Button title="Genearte Report Sheet" />
+					<Button title="Generate Report Sheet" />
 				</View>
 			</View>
 		</View>
