05b4432b4d2c2917619fd7e87d3394a1dd9e9f98
Raja <srajasep27@gmail.com>
2023-12-09 12:25:37 +0530

    changed git ignore and moved constants file into constants folder

---
+++ src/constants/constants.js
@@ -0,0 +1,11 @@
+const menu = [
+	{ id: '1', label: 'add client', icon: 'user-plus', to: 'AddClient' },
+	{ id: '2', label: 'add orders', icon: 'plus', to: 'AddOrders' },
+	{ id: '3', label: 'edit orders', icon: 'eraser', to: 'EditOrder' },
+	{ id: '4', label: 'view clients', icon: 'eye', to: 'ViewClients' },
+	{ id: '5', label: 'view menu', icon: 'user-plus', to: 'ViewMenu' },
+	{ id: '6', label: 'Menu quotation', icon: 'dropbox', to: 'MenuQuotation' },
+	{ id: '7', label: 'Generate Invoice', icon: 'book', to: 'IRgeneration' },
+];
+
+export default menu;
